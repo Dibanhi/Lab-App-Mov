c044bbd2a9e62ea4a5ba50195f2b4a2c06c775af
Dibanhi Rodriguez Bravo <57021999+Dibanhi@users.noreply.github.com>
2020-10-13 22:39:32 -0500

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+  const routes: Routes = [
+    
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'lugares',
+    loadChildren: () => import('./lugares/lugares.module').then( m => m.LugaresPageModule)
+  },
+  {
+    path: 'reservaciones',
+    loadChildren: () => import('./reservaciones/reservaciones.module').then( m => m.ReservacionesPageModule)
+  },
+  
+  ];
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
